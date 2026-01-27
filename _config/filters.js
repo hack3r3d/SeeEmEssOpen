7757e668f85ea4899768ebe311f4296345c4daa1
hack3r3d <this@1200tech.com>
2026-01-26 23:31:16 -0500

    version 1.0.3

--- _config/filters.js
+++ _config/filters.js
@@ -1,5 +1,5 @@
 import { DateTime } from "luxon";
-
+/**  */
 export default function(eleventyConfig) {
 	eleventyConfig.addFilter("readableDate", (dateObj, format, zone) => {
 		// Formatting tokens for Luxon: https://moment.github.io/luxon/#/formatting?id=table-of-tokens
